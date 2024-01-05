221cd732c6d54a88024239ed9631e51b46097d05
cuongho <hothanh.cuong45@gmail.com>
2024-01-05 23:06:12 +0700

    start front end

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,10 @@
+
+export default function Layout() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+        </h1>
+    </div>
+  )
+}
